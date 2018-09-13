8d0bc683c57633e4028ab50df6ede1fbfed4c60a
RobMargaryan <robert@margaryan.com>
2018-09-13 01:05:57 -0700

    first commit

---
+++ HMW_2.js
@@ -0,0 +1,33 @@
+alert("hey")
+const a = 12;
+const b = 'hey';
+const c = 293;
+const d = 'hola';
+const e = 19;
+
+if (a === c) 
+{
+	console.log (b);
+}
+else if (c !== d) 
+{
+	console.log (d);
+}
+
+const f = a + c;
+const g = c - a;
+const h = 'Thanks';
+const i = 'Oops';
+if (f === g || a ===f)
+{
+	console.log (h);
+}
+else if (e === g && c !== a) 
+{
+	console.log (i);
+}
+else 
+{
+	console.log ("I'm a genius");
+}
+
